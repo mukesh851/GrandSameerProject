055131ace2b17cbf11aa3c3b4b919ff467883168
Mukesh <yadavmukesh22283@gmail.com>
2025-11-13 19:50:21 +0530

    fifth commit

--- src/components/Main/MainFile.jsx
+++ src/components/Main/MainFile.jsx
@@ -1,30 +1,31 @@
-import './MainFile.css'
-
+import './MainFile.css';
+import sameerEvents from "../../assets/mainFileVideo/sameerEvents.mp4";
 
 const MainFile = () => {
   return (
-    <>
-      <main id='main-content'>
+    <main id="main-content">
+      {/* 🔹 Background Video */}
+      <div className="video-background">
+        <video autoPlay loop muted playsInline className="background-video">
+          <source src={sameerEvents} type="video/mp4" />
+        </video>
+      </div>
 
-        <div className='hero-inner'>
-          
-            <div className='inner-content'>
-              <h1> We create <strong>Space</strong></h1>
-              <p class="intro">
-                Losberger De Boer delivers top class temporary and permanent space
-                solutions for events, commercial and public space.
-              </p>
-              <a href="" id="" class="inner-content-button">
-                Contact LDB 
-              </a>
-
-            </div>
-          
+      {/* 🔹 Foreground Content */}
+      <div className="hero-inner">
+        <div className="inner-content">
+          <h1>We create <strong>Space</strong></h1>
+          <p className="intro">
+            Losberger De Boer delivers top class temporary and permanent space
+            solutions for events, commercial and public space.
+          </p>
+          <a href="#" className="inner-content-button">
+            Contact LDB
+          </a>
         </div>
+      </div>
+    </main>
+  );
+};
 
-      </main>
-    </>
-  )
-}
-
-export default MainFile
+export default MainFile;
