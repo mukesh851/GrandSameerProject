c0c941283a7961525ff9156b0870d038a14c1c1d
Mukesh <yadavmukesh22283@gmail.com>
2026-01-18 14:44:22 +0530

    28th commit

--- src/components/Main/MainFooter.jsx
+++ src/components/Main/MainFooter.jsx
@@ -1,13 +1,14 @@
-
-import './MainFooter.css'
+// MainFooter.jsx
+import { Link } from "react-router-dom";
+import "./MainFooter.css";
 
 const MainFooter = () => {
   return (
-    <div className='main-footer-And-widget'>
+    <div className="main-footer-And-widget">
       {/* Main Footer Content */}
-      <div className='main-footer'>
-        <div className='main-footer-container'>
-          <div className='main-footer1'>
+      <div className="main-footer">
+        <div className="main-footer-container">
+          <div className="main-footer1">
             <p className="hero__featured__title">LATEST NEWS</p>
             <p className="hero__featured__text">
               Latest news dainik bhaskas business excellence award
@@ -17,12 +18,16 @@ const MainFooter = () => {
             </a>
           </div>
 
-          <div className='main-footer2'>
+          <div className="main-footer2">
             <p className="hero__featured__title">PRODUCT HIGHLIGHT</p>
             <p className="hero__featured__text">
               Product highlight Transparent aluminium hanger
             </p>
-            <a href="/global/products-services/event-space/levo/" id="footer-anchor2" className="link">
+            <a
+              href="/global/products-services/event-space/levo/"
+              id="footer-anchor2"
+              className="link"
+            >
               The Levo
             </a>
           </div>
@@ -30,25 +35,47 @@ const MainFooter = () => {
       </div>
 
       {/* Widget Section */}
-      <div className='widget'>
-        <div className='widget-container'>
-          <div className='widget-inner'>
+      <div className="widget">
+        <div className="widget-container">
+          <div className="widget-inner">
             <h2 className="widget__title">How can we help you?</h2>
+
             <ol className="quickaccess__list">
-              <li><a href="/global/solutions/commercial-space/" id="widget-commercial" className="widget-button">Design Your Own Warehouse</a></li>
-              <li><a href="/global/configurator/" id="widget-design" className="widget-button">Political & Government Events</a></li>
-              <li><a href="/global/solutions/event-space-sales/" id="widget-buy" className="widget-button">Corporate Events</a></li>
-              <li><a href="/global/solutions/event-space-rental/" id="widget-rent" className="widget-button">Concerts & Grand Weddings</a></li>
-              <li><a href="/global/solutions/event-space-rental/" id="widget-cultural" className="widget-button">Cultural and religious gathering</a></li>
+              <li>
+                <Link to="/solutions#sol-6" className="widget-button">
+                  Design Your Own Warehouse
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/solutions#sol-2" className="widget-button">
+                  Political & Government Events
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/solutions#sol-3" className="widget-button">
+                  Corporate Events
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/solutions#sol-5" className="widget-button">
+                  Concerts & Grand Weddings
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/solutions#sol-4" className="widget-button">
+                  Cultural and religious gathering
+                </Link>
+              </li>
             </ol>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainFooter
-
-
-
+export default MainFooter;
